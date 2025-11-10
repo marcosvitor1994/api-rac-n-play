02c9f360d6434e95aec5b2133b89395dde86ef4b
Marcos Vitor <marcosvitor1994@gmail.com>
2025-11-09 22:22:16 -0300

    Update cors to dashboard-cop

--- src/app.js
+++ src/app.js
@@ -14,7 +14,8 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'https://dashboard-rec-n-play.vercel.app',
-    'https://dashboard-global-citizen.vercel.app'
+    'https://dashboard-global-citizen.vercel.app',
+    'https://dashboard-cop.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
