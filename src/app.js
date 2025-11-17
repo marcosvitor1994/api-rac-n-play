8514295bd3013465b6e012f173210c03b0469ad4
Marcos Vitor <marcosvitor1994@gmail.com>
2025-11-17 18:29:40 -0300

    Fix app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(cors({
     'https://dashboard-global-citizen.vercel.app',
     'https://dashboard-cop.vercel.app',
     'https://dashboard-sest.vercel.app',
-    "https://dashboard-sest-cop30.vercel.app/"
+    "https://dashboard-sest-cop30.vercel.app"
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
